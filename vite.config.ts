a8ae3f0d95c00adb129d4a2856a4c78b7a7b4f29
effund <europeanfanfund@gmail.com>
2025-02-10 11:42:51 +0200

    Config changes for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/effund.github.io/", // Replace this with your actual repository name
   server: {
     host: "::",
     port: 8080,
